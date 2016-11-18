6a319e51038b22876c311a7ffb6fc9891faa1ed2
Christian <christian_2108@live.com>
2016-11-18 01:55:58 -0500

    Se agrego script de movimiento

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimiento : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody>(); 
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+		rb.velocity = movement * speed;
+
+	}
+}
