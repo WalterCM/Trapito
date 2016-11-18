a9fd23575ab85c54dea184ac095cfd58b54f61cb
Walter Capa <walterc316@gmail.com>
2016-11-18 18:08:27 -0500

    Se agregaron algunos colliders

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -3,17 +3,17 @@ using System.Collections;
 
 public class Movimiento : MonoBehaviour {
  
-    //public float moveForce;
-    //public float maxSpeed;
-	public float speed;
-	private Rigidbody2D rb;
+    public float moveForce;
+    public float maxSpeed;
+	//public float speed;
+	//private Rigidbody2D rb;
     private Vector3 v;
 	Animator animator;
 
     // Use this for initialization
     void Start () {
 
-		rb = GetComponent<Rigidbody2D>();
+		//rb = GetComponent<Rigidbody2D>();
 		animator = GetComponent<Animator>();
 
 	}
@@ -21,12 +21,12 @@ public class Movimiento : MonoBehaviour {
 
     void Update()
     {
-       // v = new Vector3(Input.GetAxis("Horizontal"),
-       //         Input.GetAxis("Vertical"), 0.0f);
+        v = new Vector3(Input.GetAxis("Horizontal"),
+                Input.GetAxis("Vertical"), 0.0f);
 
 		if(Input.GetKey (KeyCode.W))
 		{
-			transform.Translate(Vector2.up * speed * Time.deltaTime);
+			//transform.Translate(Vector2.up * speed * Time.deltaTime);
 			animator.SetBool("Bool7", true);
 			animator.SetBool("Bool6", true);
 			animator.SetBool("Bool8", false);
@@ -44,7 +44,7 @@ public class Movimiento : MonoBehaviour {
 
 		if (Input.GetKey (KeyCode.S)) 
 		{
-			transform.Translate(Vector2.down * speed * Time.deltaTime);
+			//transform.Translate(Vector2.down * speed * Time.deltaTime);
 			animator.SetBool("Bool1", true);
 			animator.SetBool("Bool2", true);
 			animator.SetBool("Bool8", true);
@@ -60,7 +60,7 @@ public class Movimiento : MonoBehaviour {
 
 		if (Input.GetKey (KeyCode.A)){
 
-			transform.Translate(Vector2.left * speed * Time.deltaTime);
+			//transform.Translate(Vector2.left * speed * Time.deltaTime);
 			animator.SetBool("Bool4", true);
 			animator.SetBool("Bool5", true);	
 			animator.SetBool("Bool1", false);
@@ -76,7 +76,7 @@ public class Movimiento : MonoBehaviour {
 
 		if (Input.GetKey (KeyCode.D)) 
 		{
-			transform.Translate(Vector2.right * speed * Time.deltaTime);
+			//transform.Translate(Vector2.right * speed * Time.deltaTime);
 			animator.SetBool("Bool3", true);
 			animator.SetBool("Bool6", true);	
 			animator.SetBool("Bool1", false);
@@ -97,8 +97,7 @@ public class Movimiento : MonoBehaviour {
 
     void FixedUpdate()
     {
-       // GetComponent<Rigidbody2D>().velocity = Vector3.ClampMagnitude(GetComponent<Rigidbody2D>().velocity, maxSpeed);
-       // GetComponent<Rigidbody2D>().AddForce(v.normalized * moveForce); 
-
+        GetComponent<Rigidbody2D>().velocity = Vector3.ClampMagnitude(GetComponent<Rigidbody2D>().velocity, maxSpeed);
+        GetComponent<Rigidbody2D>().AddForce(v.normalized * moveForce); 
    }
 }
