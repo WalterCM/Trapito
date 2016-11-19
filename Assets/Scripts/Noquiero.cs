1ffd716c480e915df21c61cdb2d4027449816f65
Walter Capa <walterc316@gmail.com>
2016-11-19 16:29:43 -0500

    Se agrego la animacion del borde de la cama

--- Assets/Scripts/Noquiero.cs
+++ Assets/Scripts/Noquiero.cs
@@ -2,8 +2,51 @@
 using System.Collections;
 
 public class Noquiero : MonoBehaviour {
-    void onTriggerEnter2D(Collider2D other)
+    private Animator animator;
+    private float elapsedTime = 0;
+    private bool animationRunning = false;
+    private bool collides = false;
+    void Update()
     {
-        print("collides");
+        if (!animator)
+            return;
+   
+        if (animationRunning)
+        {
+            elapsedTime += Time.deltaTime;
+
+            foreach (AnimationClip clip in animator.runtimeAnimatorController.animationClips)
+            {
+                if (clip.name == "noquieroAnimation")
+                {
+                    if (elapsedTime > clip.length)
+                    {
+                        elapsedTime = 0;
+                        animationRunning = false;
+                        animator.SetBool("bedBorder", false);
+                    }
+                }
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            //print("collides: " + collides.ToString());
+            animator.SetBool("bedBorder", collides);
+            animationRunning = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        animator = other.GetComponent<Animator>();
+        collides = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        animator = other.GetComponent<Animator>();
+        animator.SetBool("bedBorder", false);
+        collides = false;
     }
 }
