0c7a7f51de861f1787f87059e63ce3df587d5c82
Walter Capa <walterc316@gmail.com>
2016-11-19 08:15:36 -0500

    Se agregaron los sonidos y el reloj

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+
+public class Clock : MonoBehaviour {
+
+    public int firstNumber = 6;
+    public int secondNumber = 57;
+    private float elapsedTime = 0;
+    private GameObject clock;
+	// Use this for initialization
+	void Start () {
+        //GetComponent<TextMesh>().color = Color.black;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        elapsedTime += Time.deltaTime;
+
+        GetComponent<TextMesh>().text = firstNumber.ToString().PadLeft(2, '0') + ":" + secondNumber.ToString().PadLeft(2, '0');
+
+        if (elapsedTime >= 1)
+        {
+            secondNumber++;
+            if (secondNumber == 60)
+            {
+                secondNumber = 0;
+                firstNumber++;
+            }
+                
+            elapsedTime = 0;
+        }
+	}
+}
