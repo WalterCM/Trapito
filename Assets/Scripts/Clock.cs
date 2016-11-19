53b88a858d4e67158b22e0dd92ee583a704b3d64
Walter Capa <walterc316@gmail.com>
2016-11-19 14:06:04 -0500

    Se agrego el boton y se elimnaron algunos archivos innecesarios

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -6,6 +6,8 @@ public class Clock : MonoBehaviour {
     public int firstNumber = 6;
     public int secondNumber = 57;
     private float elapsedTime = 0;
+    private int elapsedSeconds = 0;
+    private string separator = ":";
 	// Use this for initialization
 	void Start () {
     }
@@ -14,9 +16,19 @@ public class Clock : MonoBehaviour {
 	void Update () {
         elapsedTime += Time.deltaTime;
 
-        GetComponent<TextMesh>().text = firstNumber.ToString().PadLeft(2, '0') + ":" + secondNumber.ToString().PadLeft(2, '0');
-
+        GetComponent<TextMesh>().text = firstNumber.ToString().PadLeft(2, '0') + separator + secondNumber.ToString().PadLeft(2, '0');
         if (elapsedTime >= 1)
+        {
+            elapsedTime = 0;
+            elapsedSeconds++;
+        }
+
+        if (elapsedSeconds % 2 == 0)
+            separator = ":";
+        else
+            separator = " ";
+
+        if (elapsedSeconds >= 60)
         {
             secondNumber++;
             if (secondNumber == 60)
@@ -24,8 +36,13 @@ public class Clock : MonoBehaviour {
                 secondNumber = 0;
                 firstNumber++;
             }
-                
-            elapsedTime = 0;
+
+            if (firstNumber == 24)
+            {
+                firstNumber = 0;
+            }
+
+            elapsedSeconds = 0;
         }
 	}
 }
---
+++ Assets/Scripts/Noquiero.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Noquiero : MonoBehaviour {
+    void onTriggerEnter2D(Collider2D other)
+    {
+        print("collides");
+    }
+}
